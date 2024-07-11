9563569d905494da2a0bddb5a0d0400f58f544b3
kachunchan-analytics <kachunchan.a@gmail.com>
2024-07-11 14:46:06 +0800

    Updated new tools for PMP

--- tools/kaban_board.py
+++ tools/kaban_board.py
@@ -1,24 +1,55 @@
-import streamlit as st
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output, State
 
-st.title("My Kanban Board")
+app = dash.Dash(__name__)
 
-# Define columns
-columns = ["To Do", "In Progress", "Done"]
+app.layout = html.Div(children=[
+    html.H1(children="My Kanban Board"),
 
-# Create a Kanban board using Streamlit's `beta_columns`
-col1, col2, col3 = st.beta_columns(len(columns))
+    # Define columns
+    dcc.Tabs(id="tabs", value="to-do", children=[
+        dcc.Tab(label="To Do", value="to-do"),
+        dcc.Tab(label="In Progress", value="in-progress"),
+        dcc.Tab(label="Done", value="done"),
+    ]),
 
-# Add cards to each column
-for i, column in enumerate(columns):
-    st.write(f"## {column}")
-    with st.beta_container():
-        for card_title in ["Task 1", "Task 2", "Task 3"]:
-            if i == 0:
-                col1.write(f"- {card_title}")
-            elif i == 1:
-                col2.write(f"- {card_title}")
-            else:
-                col3.write(f"- {card_title}")
+    html.Div(id="tab-content"),
 
-# Add a "Add Card" button
-st.button("Add Card")
+    # Input area for new tasks
+    html.Div(children=[
+        html.Label("New Task:"),
+        dcc.Input(id="new-task-input", type="text", placeholder="Enter new task"),
+        html.Button("Add Task", id="add-task-button"),
+    ]),
+])
+
+@app.callback(
+    dash.Output("tab-content", "children"),
+    [dash.Input("tabs", "value"),
+     dash.Input("add-task-button", "n_clicks")],
+    [dash.State("new-task-input", "value"),
+     dash.State("tab-content", "children")]
+)
+def update_content(tab, n_clicks, new_task, current_content):
+    if n_clicks is None or n_clicks == 0:
+        return current_content
+
+    if tab == "to-do":
+        if current_content is None:
+            current_content = html.Ul()
+        current_content.children.append(html.Li(new_task))
+    elif tab == "in-progress":
+        if current_content is None:
+            current_content = html.Ul()
+        current_content.children.append(html.Li(new_task))
+    elif tab == "done":
+        if current_content is None:
+            current_content = html.Ul()
+        current_content.children.append(html.Li(new_task))
+
+    return current_content
+
+if __name__ == "__main__":
+    app.run_server(debug=False)
